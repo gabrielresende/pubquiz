9ce17978bdb0e2197aadf0138a964e42a5def652
Gabriel Resende <gabrielresende@gmail.com>
2020-05-11 19:36:22 +0200

    Translate home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user
+  around_action :set_locale
 
   def current_user
     @current_user ||= User.find_by(access_hash: cookies[:access_hash])
@@ -12,5 +13,17 @@ class ApplicationController < ActionController::Base
 
     @current_user = User.create!(access_hash: SecureRandom.hex(32))
     cookies[:access_hash] = { value: @current_user.access_hash, expires: 10.years.from_now }
+  end
+
+  def set_locale(&action)
+    locale = request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+
+    if locale && I18n.available_locales.include?(locale.to_sym)
+      puts 'locale BR'
+      I18n.with_locale(locale, &action)
+    else
+      puts 'locale default ' + I18n.default_locale.to_s
+      I18n.with_locale(I18n.default_locale, &action)
+    end
   end
 end
